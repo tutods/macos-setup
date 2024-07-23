a20e8598180aa978acce4ffd5e86028c6d9c2e5f
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-23 12:36:24 +0100

    fix: node

--- node/fnm.sh
+++ node/fnm.sh
@@ -1,39 +1,59 @@
+#!/bin/bash
+
 # Install fnm formula (if not installed)
-if ! brew list | grep fnm; then
+if ! brew list 2>/dev/null | grep -q 'fnm'; then
   brew install fnm
 fi
-
-eval "$(fnm env)"
 
 # Install lts version
 fnm install --lts --corepack-enabled
 
-# Enable completions and setup env
+# Set up for the current shell
 if [[ $SHELL == *"fish"* ]]; then
-    # Create the fnm.fish file with the specified content
-  echo "fnm env --use-on-cd | source" > ~/.config/fish/conf.d/fnm.fish
+  echo 'fnm env --use-on-cd | source' > ~/.config/fish/conf.d/fnm.fish
+  fnm completions --shell fish
 
-  # Source the fnm.fish file in Fish shell
-  fish -c "source ~/.config/fish/conf.d/fnm.fish"
-  # fnm completions --shell fish
-  fish
+  # Reload the terminal
+  source ~/.config/fish/conf.d/fnm.fish && fish
 elif [[ $SHELL == *"bash"* ]]; then
-  echo "SETUP FOR BASH"
   echo 'eval "$(fnm env --use-on-cd)"' >> ~/.bashrc
   fnm completions --shell bash
-
-  bash -c "source ~/.bashrc"
-  bash
+  
+  # Reload the terminal
+  source ~/.bashrc && bash
 elif [[ $SHELL == *"zsh"* ]]; then
-  echo "SETUP FOR ZSH"
   echo 'eval "$(fnm env --use-on-cd)"' >> ~/.zshrc
   fnm completions --shell zsh
 
-  zsh -c "source ~/.zshrc"
-  zsh
-else
-  fnm completions
+  # Reload the terminal
+  source ~/.zshrc && zsh
 fi
+
+
+# Enable completions and setup env
+# if [[ $SHELL == *"fish"* ]]; then
+#   fnm completions --shell fish
+#   echo "fnm env --use-on-cd | source" > ~/.config/fish/conf.d/fnm.fish
+
+#   fish -c "source ~/.config/fish/conf.d/fnm.fish"
+#   fish
+# elif [[ $SHELL == *"bash"* ]]; then
+#   echo "SETUP FOR BASH"
+#   echo 'eval "$(fnm env --use-on-cd)"' >> ~/.bashrc
+#   fnm completions --shell bash
+
+#   bash -c "source ~/.bashrc"
+#   bash
+# elif [[ $SHELL == *"zsh"* ]]; then
+#   echo "SETUP FOR ZSH"
+#   echo 'eval "$(fnm env --use-on-cd)"' >> ~/.zshrc
+#   fnm completions --shell zsh
+
+#   zsh -c "source ~/.zshrc"
+#   zsh
+# else
+#   fnm completions
+# fi
 
 # Use version
 echo "USE VERSION"
