bf0dbc31dfef9f70cca6bd4da9c49541d5de50fc
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-23 12:17:05 +0100

    fix: node

--- node/fnm.sh
+++ node/fnm.sh
@@ -6,25 +6,27 @@ fi
 eval "$(fnm env)"
 
 # Enable completions and setup env
-# if [[ $SHELL == *"fish"* ]]; then
-#   fnm completions --shell fish
-#   touch ~/.config/fish/conf.d/fnm.fish
-#   echo "fnm env --use-on-cd | source" >> ~/.config/fish/conf.d/fnm.fish
+if [[ $SHELL == *"fish"* ]]; then
+  fnm completions --shell fish
+  touch ~/.config/fish/conf.d/fnm.fish
+  echo "fnm env --use-on-cd | source" >> ~/.config/fish/conf.d/fnm.fish
 
-#   source ~/.config/fish/conf.d/fnm.fish
-# elif [[ $SHELL == *"bash"* ]]; then
-#   fnm completions --shell bash
-#   echo 'eval "$(fnm env --use-on-cd)"' >> ~/.bashrc
+  source ~/.config/fish/conf.d/fnm.fish
+elif [[ $SHELL == *"bash"* ]]; then
+  echo "SETUP FOR BASH"
+  echo 'eval "$(fnm env --use-on-cd)"' >> ~/.bashrc
 
-#   source ~/.bashrc
-# elif [[ $SHELL == *"zsh"* ]]; then
-#   fnm completions --shell zsh
-#   echo 'eval "$(fnm env --use-on-cd)"' >> ~/.zshrc
+  source ~/.bashrc
+  fnm completions --shell bash
+elif [[ $SHELL == *"zsh"* ]]; then
+  echo "SETUP FOR ZSH"
+  echo 'eval "$(fnm env --use-on-cd)"' >> ~/.zshrc
 
-#   source ~/.zshrc
-# else
-#   fnm completions
-# fi
+  source ~/.zshrc
+  fnm completions --shell zsh
+else
+  fnm completions
+fi
 
 # Install lts version
 fnm install --lts --corepack-enabled
