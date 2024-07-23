1471a8c052471cc867d93add428ee51d33ea828b
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-23 12:12:41 +0100

    fix: node

--- node/fnm.sh
+++ node/fnm.sh
@@ -6,18 +6,25 @@ fi
 eval "$(fnm env)"
 
 # Enable completions and setup env
-if [[ $SHELL == *"fish"* ]]; then
-  fnm completions --shell fish
-  touch ~/.config/fish/conf.d/fnm.fish
-  echo -e "fnm env --use-on-cd | source" >> ~/.config/fish/conf.d/fnm.fish
-elif [[ $SHELL == *"bash"* ]]; then
-  fnm completions --shell bash
-elif [[ $SHELL == *"zsh"* ]]; then
-  fnm completions --shell zsh
-else
-  fnm completions
-fi
-fnm env
+# if [[ $SHELL == *"fish"* ]]; then
+#   fnm completions --shell fish
+#   touch ~/.config/fish/conf.d/fnm.fish
+#   echo "fnm env --use-on-cd | source" >> ~/.config/fish/conf.d/fnm.fish
+
+#   source ~/.config/fish/conf.d/fnm.fish
+# elif [[ $SHELL == *"bash"* ]]; then
+#   fnm completions --shell bash
+#   echo 'eval "$(fnm env --use-on-cd)"' >> ~/.bashrc
+
+#   source ~/.bashrc
+# elif [[ $SHELL == *"zsh"* ]]; then
+#   fnm completions --shell zsh
+#   echo 'eval "$(fnm env --use-on-cd)"' >> ~/.zshrc
+
+#   source ~/.zshrc
+# else
+#   fnm completions
+# fi
 
 # Install lts version
 fnm install --lts --corepack-enabled
