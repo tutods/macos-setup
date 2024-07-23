07cd8450edc6a7c16becfa80453252d54310c270
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-23 13:50:23 +0100

    feat: update fish configs

--- fish/scripts/config.sh
+++ fish/scripts/config.sh
@@ -1,7 +1,11 @@
 DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
 CONFIG_FILE="$DIR/configs/config.fish"
 KEY_BINDINGS_FILE="$DIR/configs/fish_user_key_bindings.fish"
+FUNCTION_FILES="$DIR/configs/functions"
+CONF_D_FILES="$DIR/configs/conf-d"
 
 # Copy config files
 cp -f $CONFIG_FILE ~/.config/fish/config.fish
 cp -f $KEY_BINDINGS_FILE ~/.config/fish/functions/fish_user_key_bindings.fish
+cp -r $FUNCTION_FILES ~/.config/fish/functions/
+cp -r $CONF_D_FILES ~/.config/fish/conf.d/
--- fish/scripts/install.sh
+++ fish/scripts/install.sh
@@ -1,15 +1,15 @@
 # Check if fish is installed, if not install it
-if ! brew list | grep fish; then
+if ! brew list 2>/dev/null | grep -q fish; then
   brew install fish
 fi
 
 # Check if fuzzy search is installed, if not install it
-if ! brew list | grep fzf; then
+if ! brew list 2>/dev/null | grep -q fzf; then
   brew install fzf
 fi
 
 # Check if zoxide is installed, if not install it
-if ! brew list | grep zoxide; then
+if ! brew list 2>/dev/null | grep -q zoxide; then
   brew install zoxide
 fi
 
