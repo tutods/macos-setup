194aad09778601a97f8ed93ec831f618282a0937
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-23 13:34:08 +0100

    refactor: fnm script

--- node/fnm.sh
+++ node/fnm.sh
@@ -15,30 +15,28 @@ case $(basename $SHELL) in
     fish)
         echo "Fish shell detected"
         echo 'fnm env --use-on-cd | source' > ~/.config/fish/conf.d/fnm.fish
-        fish
+        source ~/.config/fish/conf.d/fnm.fish
         ;;
     zsh)
         echo "Zsh shell detected"
         echo 'eval "$(fnm env --use-on-cd)"' >> ~/.zshrc
-        zsh
+        source ~/.zshrc
         ;;
     bash)
         echo "Bash shell detected"
         echo 'eval "$(fnm env --use-on-cd)"' >> ~/.bashrc
-        bash
+        source ~/.bashrc
         ;;
     *)
         echo "Unknown shell: $(basename $SHELL)"
         ;;
 esac
 
-
+eval "$(fnm env --use-on-cd)"
 # Set up for the current shell
+echo "3) Setup shell completions"
 fnm completions --log-level quiet --shell $(basename $SHELL)
 
-# Use version
-echo "3) Set default version to use"
-fnm default $(fnm ls | head -n 1)
 
 # Enable Corepack (for Yarn and PNPM)
 echo "4) Enable Corepack"
