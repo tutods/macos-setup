3d6f0ccfb3891cafd182beb3b5997b0bfdc03deb
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-26 14:27:44 +0100

    fix: omf

--- fish/scripts/omf.sh
+++ fish/scripts/omf.sh
@@ -6,7 +6,8 @@ fish -c "curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin
 
 # Copy OMF config
 cp -f $OMF_FILE ~/.config/fish/conf.d
+source $HOME/.config/fish/config.fish
 
 # Install plugins
-fish -c "omf install git"
+fish -c "omf install https://github.com/jhillyerd/plugin-git"
 fish -c "omf install pisces"
