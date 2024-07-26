17dd429875b5ce5732a9f342f1f93e6a89149eae
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-26 11:09:58 +0100

    fix: copy commands

--- fish/scripts/config.sh
+++ fish/scripts/config.sh
@@ -1,11 +1,9 @@
 DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
-CONFIG_FILE="$DIR/configs/config.fish"
-KEY_BINDINGS_FILE="$DIR/configs/fish_user_key_bindings.fish"
-FUNCTION_FILES="$DIR/configs/functions"
-CONF_D_FILES="$DIR/configs/conf-d"
+CONFIG_FILE="./configs/config.fish"
+FUNCTION_FILES="./configs/functions"
+CONF_D_FILES="./configs/conf.d"
 
 # Copy config files
 cp -f $CONFIG_FILE ~/.config/fish/config.fish
-cp -f $KEY_BINDINGS_FILE ~/.config/fish/functions/fish_user_key_bindings.fish
-cp -r $FUNCTION_FILES ~/.config/fish/functions/
-cp -r $CONF_D_FILES ~/.config/fish/conf.d/
+cp -r $FUNCTION_FILES ~/.config/fish/
+cp -r $CONF_D_FILES ~/.config/fish/
