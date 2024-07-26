c1252b279a6199b0373cab2b27f3f926bf164cd7
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-26 13:51:39 +0100

    chore: add fish to options

--- fish/scripts/omf.sh
+++ fish/scripts/omf.sh
@@ -4,9 +4,9 @@ OMF_FILE="$DIR/configs/omf.fish"
 # Install OMF
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
+# Copy OMF config
+cp -f $OMF_FILE ~/.config/fish/conf.d
+
 # Install plugins
 omf install git
 omf install pisces
-
-# Copy OMF config
-cp -f $OMF_FILE ~/.config/fish/conf.d/omf.fish
