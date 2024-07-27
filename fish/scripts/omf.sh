31985d8e70117e8931c3993dd17d272b878efd95
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-27 17:59:32 +0100

    fix: omf

--- fish/scripts/omf.sh
+++ fish/scripts/omf.sh
@@ -2,7 +2,9 @@ DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
 OMF_FILE="$DIR/configs/omf.fish"
 
 # Install OMF
-eval "$(curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish)"
+# curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install
+
+eval"$(curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install && fish install --path=~/.local/share/omf --config=~/.config/omf)"
 
 # eval "$(source $HOME/.config/fish/config.fish)"
 # eval "$(source $HOME/.config/fish/conf.d/omf.fish)"
