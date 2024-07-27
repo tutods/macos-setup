048535b5fa2b26447674fa3dc80daa3a013a9b08
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-27 18:58:38 +0100

    fix: fisher

--- fish/scripts/fisher.sh
+++ fish/scripts/fisher.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 # Install OMF
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish > $HOME/.local/fisher.fish
-eval "$(source $HOME/.local/fisher.fish)"
-eval "$(fisher install jorgebucaran/fisher)"
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+source ~/.config/fish/functions/fisher.fish
 
-# rm -rf $HOME/.local/fisher.fish
-
-# curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
-# fish install --path=~/.local/share/omf --config=~/.config/omf
-# Wait for the installation to complete before proceeding
-
-# eval "$(source $HOME/.config/config.fish)"
-# eval "$(source $HOME/.config/conf.d/omf.fish)"
-# eval "$(fish $HOME/.local/install --path=$HOME/.local/share/omf --config=$HOME/.config/omf)"
-# rm $HOME/.local/install
-# eval "$(source $OMF_PATH/init.fish)"
-
-# Copy OMF config
-# cp -f $OMF_FILE ~/.config/fish/conf.d
-# eval "$(source $HOME/.config/fish/config.fish)"
+fisher install jorgebucaran/fisher
 
 # Install plugins
 fisher install jhillyerd/plugin-git
