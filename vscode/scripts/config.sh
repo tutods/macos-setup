bd2551fb951bbb541349a436a323817f300e4c67
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-27 20:15:30 +0100

    fix(vscode): copy config

--- vscode/scripts/config.sh
+++ vscode/scripts/config.sh
@@ -2,4 +2,6 @@ DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
 SETTINGS_FILE="$DIR/configs/settings.json"
 
 # Replace config file
-cp -f $SETTINGS_FILE $HOME/Library/Application\ Support/Code/User/
+cat $SETTINGS_FILE > $HOME/Library/Application\ Support/Code/User/settings.json
+
+~/Library/Application Support/Code/User/settings.json
