c53b2de4923cdbdc91049995406362a306aef127
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-27 18:17:51 +0100

    fix: omf

--- fish/scripts/omf.sh
+++ fish/scripts/omf.sh
@@ -9,13 +9,13 @@ curl -f https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/insta
 # eval "$(fish $HOME/.local/install --path=$HOME/.local/share/omf --config=$HOME/.config/omf)"
 # rm $HOME/.local/install
 
-fish -c "$(source $HOME/.config/fish/config.fish)"
-fish -c "$(source $HOME/.config/fish/conf.d/omf.fish)"
+fish -c "source $HOME/.config/fish/config.fish"
+fish -c "source $HOME/.config/fish/conf.d/omf.fish"
 
 # Copy OMF config
 # cp -f $OMF_FILE ~/.config/fish/conf.d
 # eval "$(source $HOME/.config/fish/config.fish)"
 
 # Install plugins
-omf install https://github.com/jhillyerd/plugin-git
-omf install pisces
+fish -c "omf install https://github.com/jhillyerd/plugin-git"
+fish -c "omf install pisces"
