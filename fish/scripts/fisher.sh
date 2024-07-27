348f8bc5c4a1651b43e9104a17372a3690425ebe
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-27 18:53:14 +0100

    fix: fisher

--- fish/scripts/fisher.sh
+++ fish/scripts/fisher.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
 DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
 OMF_FILE="$DIR/configs/omf.fish"
 
 # Install OMF
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source  && fisher install jorgebucaran/fisher
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish > $HOME/.local/fisher.fish
+fisher install jorgebucaran/fisher
+
+rm -rf $HOME/.local/fisher.fish
 
 # curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
 # fish install --path=~/.local/share/omf --config=~/.config/omf
