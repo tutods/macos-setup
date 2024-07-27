ae0f5030098690fc72afef92c926cc1e309b0be6
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-27 18:59:13 +0100

    fix: fisher

--- fish/scripts/fisher.sh
+++ fish/scripts/fisher.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # Install OMF
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-source ~/.config/fish/functions/fisher.fish
-
-fisher install jorgebucaran/fisher
+fish -c "source ~/.config/fish/functions/fisher.fish"
+fish -c "fisher install jorgebucaran/fisher"
 
 # Install plugins
-fisher install jhillyerd/plugin-git
-fisher install laughedelic/pisces
+fish -c "fisher install jhillyerd/plugin-git"
+fish -c "fisher install laughedelic/pisces"
