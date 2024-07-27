d29a694606e6594a9d4b772262b981f25f9f2f18
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-27 20:18:02 +0100

    fix(vscode): copy config

--- vscode/scripts/config.sh
+++ vscode/scripts/config.sh
@@ -2,6 +2,19 @@ DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
 SETTINGS_FILE="$DIR/configs/settings.json"
 
 # Replace config file
-cat $SETTINGS_FILE > $HOME/Library/Application\ Support/Code/User/settings.json
+DIR_PATH=$HOME/Library/Application\ Support/Code/User
+FILE_PATH=$DIR_PATH/settings.json
 
-~/Library/Application Support/Code/User/settings.json
+# Check if the file exists
+if [ ! -e "$FILE_PATH" ]; then
+    if [ ! -d "$DIR_PATH" ]; then
+        # Create the directory if it doesn't exist
+        mkdir -p "$DIR_PATH"
+    fi
+
+    # Create the file if it doesn't exist
+    touch "$FILE_PATH"
+fi
+
+
+cat $SETTINGS_FILE > $HOME/Library/Application\ Support/Code/User/settings.json
