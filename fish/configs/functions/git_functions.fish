c376385f29f70b4fa09b54a13d7ab39ad27c9448
Daniel Sousa <tutods@Daniels-iMac.local>
2024-07-27 22:48:03 +0100

    chore: update configs

--- fish/configs/functions/git_functions.fish
+++ fish/configs/functions/git_functions.fish
@@ -1,19 +1,19 @@
-function goMain
+function goMain -d "Switch to main branch"
     git checkout main
     git pull
 end
 
-function goMaster
+function goMaster -d "Switch to master branch"
     git checkout master
     git pull
 end
 
-function goBranch
+function goBranch -d "Switch to a specific branch"
     git checkout "$argv"
     git pull
 end
 
-function commt
+function commt -d "Run a full commit, stagin files, do the commit message and push"
     git add .
     git commit -m "$argv"
     git push
