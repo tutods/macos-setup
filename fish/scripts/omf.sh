3f4948ab6004802d76e79adfd5b13efa4308b0f7
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-07-27 18:40:45 +0100

    fix: omf

--- fish/scripts/omf.sh
+++ fish/scripts/omf.sh
@@ -2,7 +2,7 @@ DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
 OMF_FILE="$DIR/configs/omf.fish"
 
 # Install OMF
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
 # curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
 # fish install --path=~/.local/share/omf --config=~/.config/omf
@@ -11,8 +11,8 @@ while [ ! -d $HOME/.local/share/omf ]; do
     sleep 1
 done
 
-eval "$(source $HOME/.config/config.fish)"
-eval "$(source $HOME/.config/conf.d/omf.fish)"
+# eval "$(source $HOME/.config/config.fish)"
+# eval "$(source $HOME/.config/conf.d/omf.fish)"
 # eval "$(fish $HOME/.local/install --path=$HOME/.local/share/omf --config=$HOME/.config/omf)"
 # rm $HOME/.local/install
 # eval "$(source $OMF_PATH/init.fish)"
@@ -22,5 +22,5 @@ eval "$(source $HOME/.config/conf.d/omf.fish)"
 # eval "$(source $HOME/.config/fish/config.fish)"
 
 # Install plugins
-omf install https://github.com/jhillyerd/plugin-git
-omf install pisces
+fisher install jhillyerd/plugin-git
+fisher install pisces
