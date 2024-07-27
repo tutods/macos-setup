220258fe1b60bcaf862f0e72f6546a42b3f088e9
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2024-07-27 22:54:04 +0100

    fix: move git functions to conf.d folder

--- fish/configs/conf.d/alias.fish
+++ fish/configs/conf.d/alias.fish
@@ -10,6 +10,27 @@ alias ga="git add"
 alias grh="git reset --hard"
 alias gr="git reset"
 
+function goMain -d "Switch to main branch"
+    git checkout main
+    git pull
+end
+
+function goMaster -d "Switch to master branch"
+    git checkout master
+    git pull
+end
+
+function goBranch -d "Switch to a specific branch"
+    git checkout "$argv"
+    git pull
+end
+
+function commt -d "Run a full commit, stagin files, do the commit message and push"
+    git add .
+    git commit -m "$argv"
+    git push
+end
+
 # Homebrew
 alias bci="brew install --cask"
 alias bi="brew install"
