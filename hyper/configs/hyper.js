a3665b5431d1c66bbb4ce3e5b3ad41744a688df3
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-11-22 16:45:07 +0000

    feat(hyper - wip): partial steps

--- hyper/configs/hyper.js
+++ hyper/configs/hyper.js
@@ -144,14 +144,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [
-    "hyper-font-ligatures",
-    "hyper-search",
-    "hyper-pane",
-    "hypercwd",
-    "hyper-dracula",
-    "hyper-tabs-enhanced",
-  ],
+  plugins: [],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
---
+++ hyper/scripts/plugins.sh
@@ -0,0 +1,5 @@
+hyper i hyper-rose-pine
+hyper i hyper-statusline
+hyper i hyper-font-ligatures
+hyper i hyper-search
+hyper i hyper-pane
