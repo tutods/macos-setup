b20b23d5d9e3c60c863d57d4f18820394d31a38b
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2025-04-26 03:10:02 +0100

    chore: comments

--- nix.sh
+++ nix.sh
@@ -1,2 +1,4 @@
+# build config
 nix --extra-experimental-features 'nix-command flakes'  build ".#darwinConfigurations.$1.system"
+# apply the config
 nix run nix-darwin/master#darwin-rebuild -- switch --flake ".#$1"
