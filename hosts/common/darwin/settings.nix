c4c80d6a573e9a1b3f5c9ccede430a7dc6d8b4d5
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2025-04-26 04:16:11 +0100

    chore: finder sorting

--- hosts/common/darwin/settings.nix
+++ hosts/common/darwin/settings.nix
@@ -145,6 +145,9 @@
       ########################
       # Finder
       ########################
+      # Set sorting and sorting direction
+      defaults write com.apple.finder sortColumn -string "name"
+      defaults write com.apple.finder sortDirection -string "ascending"
       # Automatically open a new Finder window when a volume is mounted
       defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
       defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
