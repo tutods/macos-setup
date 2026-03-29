da20b6263b9cf89bd42281822b6a17e73dbfe7ef
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2026-03-29 07:46:40 +0100

    chore: activation script

--- hosts/darwin/work/default.nix
+++ hosts/darwin/work/default.nix
@@ -11,4 +11,15 @@
       homeConfig = import ../../../home/daniel.a.sousa/default.nix;
     })
   ];
+
+  # home.activation runs as admin.daniel.a.sousa on this machine, so use
+  # system.activationScripts (runs as root) to create the Developer folder
+  # with the correct owner instead.
+  system.activationScripts.createDeveloperDir.text = ''
+    if [ ! -d "/Users/daniel.a.sousa/Developer" ]; then
+      echo "↣ Create Developer directory for daniel.a.sousa"
+      mkdir -p "/Users/daniel.a.sousa/Developer"
+      chown daniel.a.sousa "/Users/daniel.a.sousa/Developer"
+    fi
+  '';
 }
